995fb65a77bc8b53ae322dee93e7476086e0b74b
Ritu Kumari <123047891+Ritukrish@users.noreply.github.com>
2025-08-19 14:34:50 +0530

    Update seq_detect_mealy.v

--- Lab2/fsm-assignments/problem1_seqdet/seq_detect_mealy.v
+++ Lab2/fsm-assignments/problem1_seqdet/seq_detect_mealy.v
@@ -1,9 +1,12 @@
 module seq_detect_mealy(
-    input clk, reset, x,
-    output reg y
+    input clk, reset, din,
+    output wire y
 );
     reg [2:0] state, next_state;
     parameter S0=0, S1=1, S2=2, S3=3;
+
+    reg y_reg;  // internal reg for output
+    assign y = y_reg; // drive wire output
 
     always @(posedge clk or posedge reset) begin
         if (reset) state <= S0;
@@ -11,14 +14,14 @@ module seq_detect_mealy(
     end
 
     always @(*) begin
-        y = 0;
+        y_reg = 0; // default
         case (state)
-            S0: if (x) next_state = S1; else next_state = S0;
-            S1: if (x) next_state = S2; else next_state = S0;
-            S2: if (~x) next_state = S3; else next_state = S2;
-            S3: if (x) begin
+            S0: if (din) next_state = S1; else next_state = S0;
+            S1: if (din) next_state = S2; else next_state = S0;
+            S2: if (~din) next_state = S3; else next_state = S2;
+            S3: if (din) begin
                     next_state = S1; 
-                    y = 1; // Mealy output
+                    y_reg = 1; // Mealy output
                 end
                 else next_state = S0;
         endcase
