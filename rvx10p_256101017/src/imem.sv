775a2ce91ad981a62ab30ebed3028d7d6aa420fc
Ritu235 <ccracker058@gmail.com>
2025-10-30 19:39:35 +0530

    Update imem.sv

--- rvx10p_256101017/src/imem.sv
+++ rvx10p_256101017/src/imem.sv
@@ -4,19 +4,19 @@
 //===========================================================
 
 module imem (
-    input  logic [31:0] addr,          // Address (from PC)
-    output logic [31:0] instr          // Output instruction
+  input  logic [31:0] a,  // Address
+  output logic [31:0] rd  // Read data (instruction)
 );
+  // Memory array (64 entries, 32-bits wide)
+  logic [31:0] RAM[63:0];
 
-    // 1024 x 32-bit instruction memory (4 KB)
-    logic [31:0] memory [0:1023];
-
-    // Initialize instruction memory from external file
-    initial begin
-        $readmemh("tests/rvx10_pipeline.hex", memory);   // Load program into memory
-    end
-
-    // Word-aligned instruction fetch
-    assign instr = memory[addr[11:2]];
+  // Initialize memory from file
+  initial begin
+    // Load the contents of "risctest.mem" into the RAM array
+    $readmemh("risctest.mem", RAM);
+  end
+    
+  // Combinational read (uses lower bits of 'a' as word index)
+  assign rd = RAM[a[31:2]]; // word-aligned
 
 endmodule
